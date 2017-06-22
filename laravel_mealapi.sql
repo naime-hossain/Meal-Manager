33e98ae6af857b932c80b487b32b99d4f3758588
naime <hossain.naime@yahoo.com>
2017-06-22 16:44:15 +0600

    sql file updated

--- laravel_mealapi.sql
+++ laravel_mealapi.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 08, 2017 at 10:43 AM
+-- Generation Time: Jun 22, 2017 at 04:43 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.8
 
@@ -46,6 +46,7 @@ CREATE TABLE `bazars` (
 CREATE TABLE `members` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `meal_count` int(10) UNSIGNED DEFAULT NULL,
   `deposit` int(10) UNSIGNED DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -55,9 +56,8 @@ CREATE TABLE `members` (
 -- Dumping data for table `members`
 --
 
-INSERT INTO `members` (`id`, `name`, `deposit`, `created_at`, `updated_at`) VALUES
-(1, 'naime hossain', NULL, '2017-06-07 04:39:33', '2017-06-07 13:47:42'),
-(3, 'sakib', NULL, '2017-06-07 13:39:52', '2017-06-07 13:39:52');
+INSERT INTO `members` (`id`, `name`, `meal_count`, `deposit`, `created_at`, `updated_at`) VALUES
+(1, 'naime', 25, NULL, '2017-06-22 04:16:33', '2017-06-22 04:27:55');
 
 -- --------------------------------------------------------
 
@@ -76,12 +76,11 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(12, '2014_10_12_000000_create_users_table', 1),
-(13, '2014_10_12_100000_create_password_resets_table', 1),
-(14, '2017_06_03_190621_create_periods_table', 1),
-(15, '2017_06_03_190855_create_bazars_table', 1),
-(16, '2017_06_07_093329_create_admins_table', 1),
-(17, '2017_06_07_100056_create_members_table', 1);
+(6, '2014_10_12_000000_create_users_table', 1),
+(7, '2014_10_12_100000_create_password_resets_table', 1),
+(8, '2017_06_03_190621_create_periods_table', 1),
+(9, '2017_06_03_190855_create_bazars_table', 1),
+(10, '2017_06_07_100056_create_members_table', 1);
 
 -- --------------------------------------------------------
 
@@ -109,16 +108,6 @@ CREATE TABLE `periods` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `periods`
---
-
-INSERT INTO `periods` (`id`, `name`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'january2017', 0, '2017-06-07 05:59:42', '2017-06-07 06:03:04'),
-(2, 'january2016', 0, '2017-06-07 06:03:04', '2017-06-07 22:28:08'),
-(3, 'may2017', 0, '2017-06-07 22:26:28', '2017-06-07 22:28:08'),
-(4, 'june2017', 1, '2017-06-07 22:28:08', '2017-06-07 22:28:08');
-
 -- --------------------------------------------------------
 
 --
@@ -139,7 +128,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin@yahoo.com', '$2y$10$bSxgQjqItYhGmp6GUKzr5OH0aveJmDbmNK/qNzK17EQ.wVQarOgja', '2017-06-07 04:38:40', '2017-06-07 04:38:40');
+(1, 'admin', 'admin@yahoo.com', '$2y$10$npbLbVW6owF8PaKjm8BfgO4JGriDZOvNAylJ/ldigYj1wgnFr2O6q', '2017-06-22 04:15:05', '2017-06-22 04:15:05');
 
 --
 -- Indexes for dumped tables
@@ -189,22 +178,22 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `bazars`
 --
 ALTER TABLE `bazars`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `members`
 --
 ALTER TABLE `members`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `periods`
 --
 ALTER TABLE `periods`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `users`
 --
