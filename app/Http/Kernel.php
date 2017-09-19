64660b3337cf7a2ccc226803ee6a97ff69d36b78
naime <hossain.naime@yahoo.com>
2017-09-19 16:50:09 +0600

    update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,9 +55,9 @@ class Kernel extends HttpKernel
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-    'jwt.auth' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
-    'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class,
-    'cors' => \App\Http\Middleware\Cors::class, 
+      'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+      'jwt.auth' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
+      'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class,
+      'cors' => \App\Http\Middleware\Cors::class, 
     ];
 }
--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class Cros
+class Cors
 {
     /**
      * Handle an incoming request.
